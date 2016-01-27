339490b0699864faedfce41ab05fb8677ccac1bb
xbasara <xbasara@gmail.com>
2016-01-27 14:12:50 -0800

    adding UI

---
+++ Source/Veil/PaperPlayerMovementComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "PaperPlayerMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VEIL_API UPaperPlayerMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/Veil/Veil.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Veil.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Veil, "Veil" );
