c69ca899830ab3ca0561610275cf4bdd8d107180
xbasara <xbasara@gmail.com>
2016-05-17 20:53:40 -0700

    added enum for movement direction

---
+++ Source/Veil/MovementEnum.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/UserDefinedEnum.h"
+#include "MovementEnum.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VEIL_API UMovementEnum : public UUserDefinedEnum
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
